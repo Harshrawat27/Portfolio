640e9842fdb18a06cff527b5286df090157c43c0
Harsh Rawat <harshrawat.dev@gmail.com>
2025-03-25 13:40:12 +0530

    saving layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Developer & Solopreneur',
   description: 'Portfolio and blog of an indie hacker and solopreneur',
-  generator: 'v0.dev',
 };
 
 export default function RootLayout({
